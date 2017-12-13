b09b87053e6faae9709a729b560638a5921705db
jisheng6 <1278661505@QQ>
2017-12-13 09:36:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/adminjs/jiaosheng1213/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.adminjs.jiaosheng1213;
+import android.os.Bundle;
+
+public class MainActivity extends TitleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("标题栏");
+        showBackwardView(R.string.text_back,true);
+        showForwardView(R.string.text_forward,true);
+    }
+}
